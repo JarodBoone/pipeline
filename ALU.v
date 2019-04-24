28cef153fc51cbd1fbc87a17cb264b790e5d1839
Boone <jboone@ad.brown.edu>
2019-04-23 20:13:42 -0400

    implemented multiplier megafunction

--- ALU.v
+++ ALU.v
@@ -1,18 +1,32 @@
-module ALU(cs,x1,x2,funct7,funct3,out,zero); 
+// The ALU does most of the work in any given computation. Ideally we 
+// would do as little muxing as possible here. Trying to parse this out to 
+// combinational logic would be pretty difficult so I'm not going to try, 
+// for clarity I'm going to define some itype macros 
+	`define RTYPE 3'b011
+	`define ITYPE 3'b001
+	`define LOAD 3'b000
+	`define STORE 3'b010
+	`define BRANCH 3'b110 
+// The itype input signal will always be one of these 5 "instruction" types
+// each of which will map to an arithmetic computation or set of arithmetic 
+// computations. In the case where we are mapped to a set by the itype we use 
+// the funct7 and funct3 values to further determine which particular ALU 
+// function we need 
+module ALU(itype,x1,x2,funct7,funct3,out,zero); 
 
 	// =========== I/O ============
-	input wire [2:0] cs; // control signal 
+	input wire [2:0] itype; // the instruction type parsed by the iDecoder 
 	input wire signed [31:0] x1,x2; // inputs 
-	input wire [6:0] funct7; 
-	input wire [2:0] funct3; 
+	input wire [6:0] funct7; // the funct7 field of the instruction 
+	input wire [2:0] funct3; // the funct3 field of the instruction 
 	
 	output reg signed [31:0] out; // output number
-	// output reg overflow; // overflow flag 
 	output wire zero; // zero flag 
 	
 	// =========== Internals ============
 	wire x1_neg, x2_neg, out_neg;
-	wire [31:0] shift_result; 
+	wire signed [31:0] shift_result; // result of the shifting megafunction 
+	wire signed [64:0] mult_result; // restult of the multiplication megafunction
 	
 	// negative value indicators 
 	assign zero = (out == 0); 
@@ -23,60 +37,56 @@ module ALU(cs,x1,x2,funct7,funct3,out,zero);
 		.result(shift_result)
 	);
 	
+	multiplier mult(
+		.dataa(x1), 
+		.datab(x2), 
+		.result(mult_result)
+	);
+	
 	// primary logic block 
 	always @* begin 
-		case (cs) 
+		case (itype) 
 			// Load or store instruction type 
-			3'b000, 3'b010 : begin // if it is a load or a store we need to perform an add 
+			`LOAD, `STORE : begin // if it is a load or a store we need to perform an add 
 				out <= x1 + x2; // add
-				// overflow = ~(~(x1_neg ^ x2_neg) & (out_neg == x1_neg)); // determine if there was overflow 
 			end 
 			// Branch or Jal instruction type 
-			3'b110 : begin // if it is a subtract 
+			`BRANCH : begin // if it is a subtract 
 				// eventually will need to case jal and jalr in here, I think they need to be an add? 
 				out <= x1 - x2; // subtract 
-				// overflow = ((x1_neg ^ x2_neg) & (out_neg == x1_neg) ); // determine if there was overflow 
 			end 
-			3'b011 , 3'b001 : begin // if it is an R or I type we need to figure out what operation based off funct7 
+			`RTYPE , `ITYPE : begin // if it is an R or I type we need to figure out what operation based off funct7 
 				if (funct7 == 7'b0100000) begin 
 					out <= x1 - x2; // subtract 
-					// overflow = ((x1_neg ^ x2_neg) & (out_neg == x1_neg) ); // determine if there was overflow 
 				end else begin 
 					case (funct3)
 						3'b000 : begin 
 							out <= x1 + x2; // add
 							if (funct7 == 7'b0000001) begin 
-								out <= x1 * x2; // multiply 
-								// overflow = 0; 
+								out <= mult_result[31:0]; // multiply 
 							end 
-							// overflow = ~(~(x1_neg ^ x2_neg) & (out_neg == x1_neg)); // determine if there was overflow 
 						end 
 						
 						3'b001 : begin 
 							out <= shift_result; // Left shift  
-							// overflow = 0; 
 						end 
 						
 						3'b110 : begin 
 							out <= x1 | x2; // bitwise OR 
-							// overflow = 0; 
 						end 
 						
 						3'b111 : begin 
 							out <= x1 & x2; // bitwise AND 
-							// overflow = 0; 
 						end 
 						
 						default : begin 
 							out <= 0; 
-							// overflow = 0; 
 						end
 					endcase // end funct3 case
 				end // end if Not SUB or MUL
 			end // end if R or I type case
 			default : begin 
 				out <= 0; 
-				// overflow = 0; 
 			end
 		endcase 
 	end 
