c4efb835190fd1097c98ccdd6f7115c53234aa16
Derman <cderman@ad.brown.edu>
2019-04-25 17:00:01 -0400

    branching fixed speed is 50 mhz

--- branch_logic.v
+++ branch_logic.v
@@ -9,14 +9,18 @@ module branch_logic(clk,jal,branch,imm,arith,zero,funct3,jump,next,PC,jalr);
 	input wire signed [31:0] PC; 
 	input wire jalr; 
 	
-	output reg jump;
-	output reg[31:0] next; 
-	
+	output wire jump;
+	output wire[31:0] next; 
+	/*
 	initial begin 
 		jump <= 0; 
 		next <= 0; 
 	end 
+	*/
+	assign next = (jal) ? ($signed(imm) >>> 2) + PC : (jalr) ? arith  : (branch) ?  ($signed(imm) >>> 2) + PC : 0;
+	assign jump = (jal|jalr) ? 1 : (branch) ? ((|funct3)^zero) : 0;
 
+	/*
 	always @(negedge clk) begin 
 		if (branch) begin 
 			if (jal) begin 
@@ -34,5 +38,5 @@ module branch_logic(clk,jal,branch,imm,arith,zero,funct3,jump,next,PC,jalr);
 			next <= 0; 
 		end 
 	end 
-
+*/
 endmodule 
